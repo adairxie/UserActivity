cbc7ecec9e369b9dbbd4c77ce0d1ce6d7837a66c
xiehui <xiehui@yundun.com>
2018-09-14 14:21:44 +0800

    bugfix

--- main.py
+++ main.py
@@ -72,5 +72,5 @@ if __name__ == '__main__':
     userScore.Run()
 
     sched = BlockingScheduler()
-    sche.add_job(timer_job, "interval", hours=23)
+    sched.add_job(timer_job, "interval", hours=23)
     sched.start()
